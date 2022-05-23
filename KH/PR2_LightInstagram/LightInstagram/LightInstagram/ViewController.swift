04cf1b610ef4b6c5c2bb3d8bc61e935d7db19f63
DHGIGHSOUL <highsoul1227@gmail.com>
2022-05-23 10:29:24 +0900

    Project 2 init

---
+++ KH/PR2_LightInstagram/LightInstagram/LightInstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightInstagram
+//
+//  Created by ROLF J. on 2022/05/23.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
