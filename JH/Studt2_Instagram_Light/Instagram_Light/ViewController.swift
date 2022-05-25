daf708d704930af10df8c38c883b66a12fdd2410
jaehyeoklim <jaehyeoklim99@gmail.com>
2022-05-26 02:52:39 +0900

    Code로 View 구현을 위한 간략화 연습

---
+++ JH/Studt2_Instagram_Light/Instagram_Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram_Light
+//
+//  Created by Jaehyeok Lim on 2022/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBlue
+    }
+
+
+}
+
