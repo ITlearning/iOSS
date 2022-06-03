d9be53645b4862633f12afce0a05354f6350e4bb
jaehyeoklim <jaehyeoklim99@gmail.com>
2022-06-03 19:34:08 +0900

    Main, Add, User View 구현 & TabBar View 구현

---
+++ JH/Study2_Instagram_Light/Pods/Target Support Files/Pods-Instagram_Light-Instagram_LightUITests/Pods-Instagram_Light-Instagram_LightUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Instagram_Light_Instagram_LightUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Instagram_Light_Instagram_LightUITests
+@end
