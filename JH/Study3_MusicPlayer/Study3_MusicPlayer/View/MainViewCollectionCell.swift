e17bc1d64a00f2dcc8418b9e9d1caedaa5c0dd27
jaehyeoklim <jaehyeoklim99@gmail.com>
2022-06-30 01:23:45 +0900

    First Setting Commit

---
+++ JH/Study3_MusicPlayer/Study3_MusicPlayer/View/MainViewCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewCollectionCell.swift
+//  Study3_MusicPlayer
+//
+//  Created by Jaehyeok Lim on 2022/06/30.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewCollectionCell: UICollectionViewCell {
+    let identifier = "MainViewCollectionCell"
+}
---
+++ JH/Study3_MusicPlayer/Study3_MusicPlayer/ViewController/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Study3_MusicPlayer
+//
+//  Created by Jaehyeok Lim on 2022/06/30.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+
+}
+
